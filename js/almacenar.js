c5046336dc02c48a4c8dc47ef2bc094558d8f300
Lihuén <110670239+lud-lihuen@users.noreply.github.com>
2022-08-26 08:48:18 -0300

    lines

--- js/almacenar.js
+++ js/almacenar.js
@@ -5,19 +5,15 @@ const item = document.getElementById("item");
 document.addEventListener("DOMContentLoaded",()=>{
     let items = JSON.parse(localStorage.getItem("items"));
     for (let i = 0; i < items.length; i++) {
-        listado.innerHTML+=`<li>${items[i]}</li>`;
-        
-    }
-});
+        listado.innerHTML+=`<li>${items[i]}</li>`;    
+    }});
 
 document.getElementById("agregar").addEventListener("click",()=>{
     array.push(item.value);
     localStorage.setItem("items", JSON.stringify(array));
-    listado.innerHTML+=`
-    <li>${item.value}</li>`;
+    listado.innerHTML+=`<li>${item.value}</li>`;
     item.value="";
-}
-);
+});
 
 document.getElementById("limpiar").addEventListener("click",()=>{
     listado.innerHTML="";
